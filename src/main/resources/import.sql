b328da651abe67a36d6a0fb17d9820fe29324eb1
stel_ <117541590+JuanRejon@users.noreply.github.com>
2024-03-19 22:26:36 -0600

    Video #1

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', 'S21', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', 'S22', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', 'S23', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Appple', 'IPhone 14', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Apple', 'IPhone 15', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Xiaomi', 'Mi', NOW());
